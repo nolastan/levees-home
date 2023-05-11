3b6958e7eced7f514a7dbbf97ded2d0f62edece0
Stanford Rosenthal <nolastan@gmail.com>
2023-05-10 23:00:33 -0500

    also ƒix css path on build

--- build.js
+++ build.js
@@ -1,15 +1,24 @@
 const replace = require('replace-in-file');
 
-const options = {
-  files: 'public/*.html',
-  from: /images\//g,
-  to: '<?php bloginfo( stylesheet_directory ); ?>/images/',
-};
+const options = [
+  {
+    files: 'public/*.html',
+    from: /src="images\//g,
+    to: 'src="<?php bloginfo( stylesheet_directory ); ?>/images/',
+  },
+  {
+    files: 'public/*.html',
+    from: /<link rel="stylesheet" href="tailwind/g,
+    to: '<link rel="stylesheet" href="<?php bloginfo( stylesheet_directory ); ?>/tailwind',
+  }
+];
 
-try {
-  const results = replace.sync(options);
-  console.log('Replacement results:', results);
-}
-catch (error) {
-  console.error('Error occurred:', error);
-}
+options.forEach((option) => {
+  try {
+    const results = replace.sync(option);
+    console.log('Replacement results:', results);
+  }
+  catch (error) {
+    console.error('Error occurred:', error);
+  }
+});
